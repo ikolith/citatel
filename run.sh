13b4b7cc490c157988b7ec8c4f7cc01a558a421b
Git Shenanigans <gitshenanigans@gmail.com>
2023-01-05 18:42:58 -0800

    streamline run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 source .venv/bin/activate
-quarto render
+#quarto render #A call to render is unneeded, as preview calls render implicitly for the pages that need to be updated, it seems.
 quarto preview
