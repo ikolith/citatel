683bfce00a876e4fcc9c4772750931f7cec4f7d9
wyattscarpenter <wyattscarpenter@gmail.com>
2023-01-27 18:36:11 -0800

    gussy up loric_transcriber

--- book/p3_worldbuilding/language/Transcriber/loric_transcribe.py
+++ book/p3_worldbuilding/language/Transcriber/loric_transcribe.py
@@ -1,25 +1,28 @@
-# type: ignore
+#!/usr/bin/env python3
+r"""LORIC_TRANSCRIBE (this is possibly a bad name. perhaps we should rename it to "fancify romanization". Or perhaps we should implement de-fancification as well...)
+A script for converting from the ASCII (normal keyboard) romanization scheme to the "official" one that includes diacritics for long vowels and geminate consonants. Can output as unicode text or TeX code.
 
-#   A script for converting from the ASCII (normal keyboard) romanization scheme to the "official" one
-#   that includes diacritics for long vowels and geminate consonants
-#   Can output as unicode text or TeX code
+SYNTAX:
+Make sure input file is in same directory, then:
+  python3 transcribe.py -i input-file -o output-file
+If no input file is specified, will read input from the first argument. Note: this means, perhaps unfortunately, that if you pass in only the flags -o whatever, this program will attempt to read from a file called -o.
+If no output file is specified, will output to tr-[input argument].
+If - is provided, standard in or out will be used as the sole input or output source, respectively. If you actually need to output to a file called -, use ./- or something.
 
-# SYNTAX:
-#   make sure input file is in same directory, then:
-#   python3 transcribe.py -i input-file -o output-file
-#   if no output file is specified, will output to tr-input-file.txt
-
-# optional flags:
-#   -v gives output on command line as well as in file (not recommended for large files)
-#   -l converts to LaTex formatting instead of unicode using the tipa package syntax
-#   make sure \usepackage{tipa} is in the header
-
+Optional flags:
+  -t [text] give input text directly by a command line argument
+  -v gives output on command line as well as in file (not recommended for large files).
+  -l converts to LaTex formatting instead of unicode using the tipa package syntax. Make sure \usepackage{tipa} is in the header of the tex file you are using this for."""
+# Hypothetically, we could allow a flag -d that converts from unicode romanization to ascii romanization, but this could be somewhat annoying to investigate the unicode implications of.
 
 import sys
 import re
 
-args = [a.lower() for a in sys.argv]
-input_filename = ""
+if len(sys.argv) <= 1:
+    print(__doc__)
+    exit()
+
+args = sys.argv
 
 if "-i" in args:
     input_filename = args[args.index("-i") + 1]
@@ -32,28 +35,40 @@ else:
     output_filename = "tr-" + input_filename
 
 
-def unicodify(input_string):
-    input_string = re.sub(r"(?<=([aiueoAIUEO]))\1", "\u0304", input_string)
-    input_string = re.sub(r"(?<=([a-zA-Z]))\1", "\u0323", input_string)
+def unicodify(input_string: str) -> str:
+    input_string = re.sub(
+        r"(?<=([aiueoAIUEO]))\1", "\u0304", input_string, flags=re.IGNORECASE
+    )
+    input_string = re.sub(
+        r"(?<=([a-zA-Z]))\1", "\u0323", input_string, flags=re.IGNORECASE
+    )
     return input_string
 
 
-def texify(input_string):
-    input_string = re.sub(r"([aiueoAIUEO])\1", r"\\textipa{\=\1}", input_string)
-    input_string = re.sub(r"(\w)\1", r"\\textipa{\.*\1}", input_string)
+def texify(input_string: str) -> str:
+    input_string = re.sub(
+        r"([aiueoAIUEO])\1", r"\\textipa{\=\1}", input_string, flags=re.IGNORECASE
+    )
+    input_string = re.sub(
+        r"(\w)\1", r"\\textipa{\.*\1}", input_string, flags=re.IGNORECASE
+    )
     return input_string
 
 
-output_text = "ERROR"
+if "-t" in args:
+    input_text = args[args.index("-t") + 1]
+else:
+    with sys.stdin if input_filename == "-" else open(
+        input_filename, "r", encoding="utf-8"
+    ) as input_file:
+        input_text = input_file.read()
+if "-l" in args:
+    output_text = texify(input_text)
+else:
+    output_text = unicodify(input_text)
 
-with open(input_filename, "r", encoding="utf-8") as input_file:
-    input_text = input_file.read()
-    if "-l" in args:
-        output_text = texify(input_text)
-    else:
-        output_text = unicodify(input_text)
-
-if "-v" in args:
+if output_filename == "-" or "-v" in args:
     print(output_text)
-with open(output_filename, "w", encoding="utf-8") as output_file:
-    output_file.write(output_text)
+if not output_filename == "-":
+    with open(output_filename, "w", encoding="utf-8") as output_file:
+        output_file.write(output_text)
