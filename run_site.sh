cad391bcc15bc2de1c6362cf72bc90ed348701fa
Git Shenanigans <gitshenanigans@gmail.com>
2023-01-27 18:44:46 -0800

    move quarto render to run_site instead

--- run_site.sh
+++ run_site.sh
@@ -1,4 +1,9 @@
-. .venv/bin/activate
 cp git_hooks/* .git/hooks/ #This is not necessary to run the site, but it is extremely useful to have this git hook copying step happen at some point, basically constantly, and there's no more-relevant script than this one to put it in.
-#quarto render #A call to render is unneeded, as preview calls render implicitly for the pages that need to be updated, it seems.
+if [ "$1" == "--dont" ] ; then #The usefulness of this option may be an indication that the current architecture is bad.
+  exit
+fi
+. .venv/bin/activate
+if [ "$1" != "--quick" ] ; then
+  quarto render #A call to render is needed to clean up any residual now-erroneous files that might be left in there, but once running preview should keep things mostly up to date
+fi
 quarto preview
